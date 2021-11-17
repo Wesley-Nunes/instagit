fe92a9c7bbc19ff793734cbf6863ce7908044f94
Wesley-Nunes <wesley_info@yahoo.com.br>
2021-11-17 18:38:12 -0300

    style: Create global styles

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import GlobalStyles from './GlobalStyles';
+import Navbar from './components/Navbar';
+
+const App = () => (
+  <>
+    <GlobalStyles />
+    <Navbar />
+  </>
+);
+
+export default App;
