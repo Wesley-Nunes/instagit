2c09afb9d5af8dd18be1500cf8fb99ce7773ca31
Wesley-Nunes <wesley_info@yahoo.com.br>
2021-11-17 20:45:09 -0300

    style: Create the style of navigation bar container

---
+++ src/components/Navbar/style.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const StyledNavBar = styled.header`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+
+  height: 6rem;
+  width: 100vw;
+
+  background-color: #fff;
+
+  border-bottom: 1px solid #dbdbdb;
+`;
+
+export default StyledNavBar;
