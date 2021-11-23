3d03cedf8891137fae462dc9f2f49a5c59693402
Wesley-Nunes <wesley_info@yahoo.com.br>
2021-11-23 15:43:53 -0300

    feat: Add conditional render
    The conditional render will return the Repository
    component or the Profile component based on click
    in the navbar buttons.
    In the future, the router will handle this state.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import {FaHome, FaSearch, FaUserCircle} from 'react-icons/fa';
 
@@ -6,18 +6,30 @@ import GlobalStyles from './GlobalStyles';
 
 import Navbar from './components/Navbar';
 import Repository from './components/Repository';
+import Profile from './components/Profile';
 
-const App = () => (
-  <>
-    <GlobalStyles />
-    <Navbar>
-      <li><a href="#" aria-label="home"><FaHome /></a></li>
-      <li><a href="#" aria-label="search"><FaSearch /></a></li>
-      <li><a href="#" aria-label="profile"><FaUserCircle /></a></li>
-    </Navbar>
-    <Repository />
-    <Repository />
-  </>
-);
+const App = () => {
+  const [isHomePage, setToHomePage] = useState(true);
+
+  return (
+    <>
+      <GlobalStyles />
+      <Navbar>
+        <li><a
+          href="#"
+          aria-label="home"
+          onClick={() => setToHomePage(true)}><FaHome /></a></li>
+        <li><a href="#" aria-label="search"><FaSearch /></a></li>
+        <li><a
+          href="#"
+          aria-label="profile"
+          onClick={() => setToHomePage(false)}><FaUserCircle /></a></li>
+      </Navbar>
+      {
+        isHomePage ? <Repository /> : <Profile />
+      }
+    </>
+  );
+};
 
 export default App;
