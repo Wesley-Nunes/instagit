6295b907d4c646a65e301c77322f491ad32dba5e
Wesley-Nunes <wesley_info@yahoo.com.br>
2021-11-30 19:09:02 -0300

    feat: The apiState handle the state of the request of GitHub API
    The function uses the useContext to access the
    available actions to change the state.
    The getUserDetails get the details of the GitHub user.
    The getRepositories get the repositories of the GitHub user.
    The search changes the GitHub user.

---
+++ src/services/stateManagement/apiState.js
@@ -0,0 +1,73 @@
+import React, {createContext, useContext, useState, useEffect} from 'react';
+
+import PropTypes from 'prop-types';
+
+import {getProfileInfo, getRepos} from '../api/githubRequest';
+
+const ApiContext = createContext({});
+
+/**
+ * Provide the state actions
+ * @param   {Array<Component>}  children
+ * @return  {Provider}
+ */
+function ApiProvider({children}) {
+  const [username, setUsername] = useState('Wesley-Nunes');
+  const [userDetails, setUserDetails] = useState({
+    name: '',
+    login: '',
+    bio: '',
+  });
+  const [respositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    getProfileInfo(username)
+        .then((response) => {
+          const {name, login, bio} = response.data;
+          setUserDetails({name, login, bio});
+        });
+
+    getRepos(username)
+        .then((response) => {
+          setRepositories(response.data);
+        });
+  }, []);
+
+  const getUserDetails = () => {
+    return userDetails;
+  };
+
+  const getRepositories = () => {
+    return respositories;
+  };
+
+  const search = (name) => {
+    setUsername(name);
+  };
+
+  const publicActions = {
+    getUserDetails,
+    getRepositories,
+    search,
+  };
+
+  return (
+    <ApiContext.Provider value={publicActions}>
+      {children}
+    </ApiContext.Provider>
+  );
+}
+
+ApiProvider.propTypes = {
+  children: PropTypes.element.isRequired,
+};
+
+/**
+ * Hooks the action of the context
+ * @return {hook} useApi
+ */
+function useApi() {
+  return useContext(ApiContext);
+}
+
+export {ApiProvider, useApi};
