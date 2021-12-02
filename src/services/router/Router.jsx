18d0ed68307c35a75234ec4499d950c4c647dd8b
Wesley-Nunes <wesley_info@yahoo.com.br>
2021-12-02 20:52:30 -0300

    feat: Create the router
    The react-router-dom handles the router management.
    The router keeps the links of navigations and
    controls what page will be rendered and what URL
    the page has to be.
    The App.jsx controls only the global styles and
    render the router.
    The package.json renders '/me/home' as the homepage.

---
+++ src/services/router/Router.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import {BrowserRouter, Route, Routes, Link, Navigate} from 'react-router-dom';
+
+import {Navbar, Search} from '../../components/shared';
+
+import {HomeIcon, UserCircleIcon} from '../icons/icons';
+import {useApi} from '../stateManagement/apiState';
+
+import {HomePage} from '../../pages/HomePage';
+import {ProfilePage} from '../../pages/ProfilePage';
+
+const Router = () => {
+  const {getUserDetails} = useApi();
+  const {login} = getUserDetails();
+
+  const basename = login === 'Wesley-Nunes' ? '/me' : `/user/${login}`;
+  return (
+    <BrowserRouter>
+      <Navbar>
+        <li>
+          <Link to={`${basename}/home`} aria-label="home">
+            <HomeIcon />
+          </Link>
+        </li>
+        <li>
+          <Search />
+        </li>
+        <li>
+          <Link to={`${basename}/profile`} aria-label="profile">
+            <UserCircleIcon />
+          </Link>
+        </li>
+      </Navbar>
+      <Routes>
+        <Route path={`${basename}/home`} element={<HomePage />}/>
+        <Route path="*" element={<Navigate replace to={`${basename}/home`} />}/>
+        <Route path={`${basename}/profile`} element={<ProfilePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
