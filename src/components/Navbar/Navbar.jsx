8c20a3edf976034c64dab45dbaf0b704fff47ed3
Paulo Martins <pfmpaulomartins@gmail.com>
2021-12-23 16:00:14 -0300

    refactor to use AvatarUser

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,30 +5,36 @@ import {Search, AvatarUser} from '..';
 import {getBasename} from '../../services/router/Router';
 import {HomeIcon} from '../../services/icons/icons';
 import {StyledTitle, StyledNavBar, StyledUl} from './NavbarStyle';
+import {useApi} from '../../services/stateManagement/apiState';
 
-const NavBar = () => (
-  <StyledNavBar>
-    <StyledTitle>
-      Instagit
-    </StyledTitle>
-    <nav>
-      <StyledUl>
-        <li>
-          <Link to={`${getBasename()}/home`} aria-label="home">
-            <HomeIcon />
-          </Link>
-        </li>
-        <li aria-label="search" tabIndex="0">
-          <Search />
-        </li>
-        <li>
-          <Link to={`${getBasename()}/profile`} aria-label="profile">
-            <AvatarUser />
-          </Link>
-        </li>
-      </StyledUl>
-    </nav>
-  </StyledNavBar>
-);
+const NavBar = () => {
+  const {getUserDetails} = useApi();
+  const {imgUrl, name} = getUserDetails();
+
+  return (
+    <StyledNavBar>
+      <StyledTitle>
+        Instagit
+      </StyledTitle>
+      <nav>
+        <StyledUl>
+          <li>
+            <Link to={`${getBasename()}/home`} aria-label="home">
+              <HomeIcon />
+            </Link>
+          </li>
+          <li aria-label="search" tabIndex="0">
+            <Search />
+          </li>
+          <li>
+            <Link to={`${getBasename()}/profile`} aria-label="profile">
+              <AvatarUser imgUrl={imgUrl} imgName={name} />
+            </Link>
+          </li>
+        </StyledUl>
+      </nav>
+    </StyledNavBar>
+  );
+}
 
 export default NavBar;
