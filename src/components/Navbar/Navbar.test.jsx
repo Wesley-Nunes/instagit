9c9d14b8eaa4762442f3fe71829b07b53d4f3d29
Wesley-Nunes <wesnmonteiro@gmail.com>
2022-03-13 19:04:47 -0300

    try: Test if the build 'll pass

--- src/components/Navbar/Navbar.test.jsx
+++ src/components/Navbar/Navbar.test.jsx
@@ -1,43 +1,59 @@
-import React from 'react';
+import React from "react";
 
-import {render, screen} from '@testing-library/react';
-import {HomeIcon, SearchIcon, UserCircleIcon} from '../../services/icons/icons';
+import { render, screen } from "@testing-library/react";
+import {
+  HomeIcon,
+  SearchIcon,
+  UserCircleIcon,
+} from "../../services/icons/icons";
 
 // import NavBar from './NavBar';
 
-describe('NavBar component', () => {
+describe("NavBar component", () => {
   beforeEach(() => {
     render(
-        <NavBar>
-          <li><a href="#" aria-label="home"><HomeIcon /></a></li>
-          <li><a href="#" aria-label="search"><SearchIcon /></a></li>
-          <li><a href="#" aria-label="profile"><UserCircleIcon /></a></li>
-        </NavBar>,
+      <NavBar>
+        <li>
+          <a href="#" aria-label="home">
+            <HomeIcon />
+          </a>
+        </li>
+        <li>
+          <a href="#" aria-label="search">
+            <SearchIcon />
+          </a>
+        </li>
+        <li>
+          <a href="#" aria-label="profile">
+            <UserCircleIcon />
+          </a>
+        </li>
+      </NavBar>
     );
   });
 
-  xit('renders a banner with the NavBar content', () => {
-    const banner = screen.getByRole('banner');
+  it("renders a banner with the NavBar content", () => {
+    const banner = screen.getByRole("banner");
     expect(banner).toBeInTheDocument();
   });
 
-  xit('renders a heading with the name "instagit"', () => {
-    const title = screen.getByRole('heading', {name: /instagit/i});
+  it('renders a heading with the name "instagit"', () => {
+    const title = screen.getByRole("heading", { name: /instagit/i });
     expect(title).toBeInTheDocument();
   });
 
-  xit('renders a link with the name "home"', () => {
-    const title = screen.getByRole('link', {name: /home/i});
+  it('renders a link with the name "home"', () => {
+    const title = screen.getByRole("link", { name: /home/i });
     expect(title).toBeInTheDocument();
   });
 
-  xit('renders a link with the name "search"', () => {
-    const title = screen.getByRole('link', {name: /search/i});
+  it('renders a link with the name "search"', () => {
+    const title = screen.getByRole("link", { name: /search/i });
     expect(title).toBeInTheDocument();
   });
 
-  xit('renders a link with the name "profile"', () => {
-    const title = screen.getByRole('link', {name: /profile/i});
+  it('renders a link with the name "profile"', () => {
+    const title = screen.getByRole("link", { name: /profile/i });
     expect(title).toBeInTheDocument();
   });
 });
